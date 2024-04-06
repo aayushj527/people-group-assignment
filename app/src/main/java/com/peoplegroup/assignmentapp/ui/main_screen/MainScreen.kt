8143e5ad39fafa22056a392a727804255ccc8768
Aayush Jain <aayush.jain@rapipay.com>
2024-04-06 18:08:39 +0530

    Added main screen

---
+++ app/src/main/java/com/peoplegroup/assignmentapp/ui/main_screen/MainScreen.kt
@@ -0,0 +1,10 @@
+package com.peoplegroup.assignmentapp.ui.main_screen
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MainScreen(
+    mainScreenViewModel: MainScreenViewModel
+) {
+
+}
---
+++ app/src/main/java/com/peoplegroup/assignmentapp/ui/main_screen/MainScreenViewModel.kt
@@ -0,0 +1,6 @@
+package com.peoplegroup.assignmentapp.ui.main_screen
+
+import androidx.lifecycle.ViewModel
+
+class MainScreenViewModel: ViewModel() {
+}
